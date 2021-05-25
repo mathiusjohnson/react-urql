72c1a605a165982c921fd0cc58e0ebe9f6363b15
Mathius <devachievem@gmail.com>
2021-05-25 15:49:01 -0700

    complete tutorial

--- src/index.js
+++ src/index.js
@@ -1,18 +1,85 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/index.css'
-import App from './components/App'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { Provider, Client, dedupExchange, fetchExchange } from 'urql'
-import { cacheExchange } from '@urql/exchange-graphcache'
+import { BrowserRouter } from 'react-router-dom';
+import { SubscriptionClient } from "subscriptions-transport-ws";
 
-const cache = cacheExchange({})
+import {
+  Provider,
+  createClient,
+  fetchExchange,
+  dedupExchange,
+  subscriptionExchange
+} from "urql";
 
-const client = new Client({
+import { cacheExchange } from '@urql/exchange-graphcache';
+
+import "./styles/index.css";
+
+import App from './components/App';
+import { FEED_QUERY } from './components/LinkList';
+import { getToken } from './token';
+
+const cache = cacheExchange({
+  updates: {
+    Mutation: {
+      post: ({ post }, _args, cache) => {
+        const variables = { first: 10, skip: 0, orderBy: "createdAt_DESC" };
+        cache.updateQuery({ query: FEED_QUERY, variables }, data => {
+          if (data !== null) {
+            data.feed.links.unshift(post);
+            data.feed.count++;
+            return data;
+          } else {
+            return null;
+          }
+        });
+      }
+    },
+    Subscription: {
+      newLink: ({ newLink }, _args, cache) => {
+        const variables = { first: 10, skip: 0, orderBy: "createdAt_DESC" };
+        cache.updateQuery({ query: FEED_QUERY, variables }, data => {
+          if (data !== null) {
+            data.feed.links.unshift(newLink);
+            data.feed.count++;
+            return data;
+          } else {
+            return null;
+          }
+        });
+      }
+    }
+  }
+});
+
+const subscriptionClient = new SubscriptionClient(
+  'ws://localhost:4000',
+  {
+    reconnect: true,
+    connectionParams: {
+      authToken: getToken()
+    }
+  }
+);
+
+const client = createClient({
   url: 'http://localhost:4000',
-  exchanges: [dedupExchange, cache, fetchExchange],
-})
+  fetchOptions: () => {
+    const token = getToken();
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : '' }
+    }
+  },
+  exchanges: [
+    dedupExchange,
+    cache,
+    fetchExchange,
+    subscriptionExchange({
+      forwardSubscription: operation => subscriptionClient.request(operation)
+    })
+  ]
+});
 
 ReactDOM.render(
   <BrowserRouter>
@@ -20,5 +87,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </BrowserRouter>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
